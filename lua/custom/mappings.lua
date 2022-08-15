f41f018e8bd817ac0d57d35e73be3494174fab25
Julien Ganichot <julien@ridedott.com>
2022-08-15 14:00:44 +0200

    fix indent

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 M.nvimtree = {
-   n = {
-      ["<leader>tm"] = { "<cmd> NvimTreeResize -5 <CR>", "?   resizes -5" },
-      ["<leader>tp"] = { "<cmd> NvimTreeResize +5 <CR>", "?   resizes -5" },
-   },
+  n = {
+    ["<leader>tm"] = { "<cmd> NvimTreeResize -5 <CR>", "?   resizes -5" },
+    ["<leader>tp"] = { "<cmd> NvimTreeResize +5 <CR>", "?   resizes -5" },
+  },
 }
 
 M.global = {
-   n = {
-      ["gd"] = { "<cmd> lua vim.lsp.buf.definition() <CR>", "?   trigger LSP formatting" },
-      ["<leader>fm"] = { "<cmd> lua vim.lsp.buf.formatting() <CR>", "?   trigger LSP formatting" },
-      ["<leader>cc"] = { "<cmd> Telescope <CR>", "?   runs Telescope" },
-      ["<leader>q"] = { "<cmd> q <CR>", "?   quits current window" },
-      ["<leader>d"] = { "<cmd> TroubleToggle <CR>", "?   toggles Trouble" },
-      ["<leader>dd"] = { "<cmd> lua vim.diagnostic.open_float() <CR>", "?   toggles local troubleshoot" },
-      ["<leader>x"] = { "<cmd> BufDel <CR>", "?   closes the current buffer" },
-      ["<leader>/"] = { "gcc", "?   comments selection" },
-   },
+  n = {
+    ["gd"] = { "<cmd> lua vim.lsp.buf.definition() <CR>", "?   trigger LSP formatting" },
+    ["<leader>fm"] = { "<cmd> lua vim.lsp.buf.formatting() <CR>", "?   trigger LSP formatting" },
+    ["<leader>cc"] = { "<cmd> Telescope <CR>", "?   runs Telescope" },
+    ["<leader>q"] = { "<cmd> q <CR>", "?   quits current window" },
+    ["<leader>d"] = { "<cmd> TroubleToggle <CR>", "?   toggles Trouble" },
+    ["<leader>dd"] = { "<cmd> lua vim.diagnostic.open_float() <CR>", "?   toggles local troubleshoot" },
+    ["<leader>x"] = { "<cmd> BufDel <CR>", "?   closes the current buffer" },
+    ["<leader>/"] = { "gcc", "?   comments selection" },
+  },
 }
 
 return M
--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,7 +1,7 @@
 local ok, null_ls = pcall(require, "null-ls")
 
 if not ok then
-   return
+  return
 end
 
 local formatters = require "custom.plugins.formatters"
@@ -9,57 +9,59 @@ local formatters = require "custom.plugins.formatters"
 local b = null_ls.builtins
 
 local sources = {
-   b.diagnostics.cspell,
+  b.diagnostics.cspell,
 
-   b.formatting.buf,
-   b.diagnostics.buf,
+  b.formatting.buf,
+  b.diagnostics.buf,
 
-   -- JS html css stuff
-   b.diagnostics.eslint_d,
-   b.code_actions.eslint_d,
-   b.formatting.prettierd.with {
-      filetypes = {
-         "html",
-         "json",
-         "markdown",
-         "scss",
-         "css",
-         "yaml",
-         "typescript",
-         "javascript",
-         "javascriptreact",
-         "vue",
-      },
-   },
+  b.formatting.rustfmt,
 
-   -- Golang
-   -- b.formatting.gofumpt,
+  -- JS html css stuff
+  b.diagnostics.eslint_d,
+  b.code_actions.eslint_d,
+  b.formatting.prettier.with {
+    filetypes = {
+      "html",
+      "json",
+      "markdown",
+      "scss",
+      "css",
+      "yaml",
+      "typescript",
+      "javascript",
+      "javascriptreact",
+      "vue",
+    },
+  },
 
-   -- Lua
-   b.formatting.stylua,
-   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+  -- Golang
+  -- b.formatting.gofumpt,
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+  -- Lua
+  b.formatting.stylua,
+  b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
 
-   -- Terraform
-   b.formatting.terraform_fmt,
+  -- Shell
+  b.formatting.shfmt,
+  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+
+  -- Terraform
+  b.formatting.terraform_fmt,
 }
 
 local M = {}
 
 M.setup = function()
-   null_ls.setup {
-      -- on_attach = function(client, bufnr)
-      --    formatters.setup(client, bufnr)
-      -- end,
-      on_attach = function(client, bufnr)
-         formatters.setup(client, bufnr)
-      end,
+  null_ls.setup {
+    -- on_attach = function(client, bufnr)
+    --    formatters.setup(client, bufnr)
+    -- end,
+    on_attach = function(client, bufnr)
+      formatters.setup(client, bufnr)
+    end,
 
-      sources = sources,
-   }
+    sources = sources,
+  }
 end
 
 return M
